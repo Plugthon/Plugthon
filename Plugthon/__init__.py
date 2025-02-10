6bbe0d5474e16f5baba69f6180a89bb0c65ed45a
iniridwanul <theridwanul@gmail.com>
2025-02-11 02:34:39 +0600

    Add Greetings, SelfDestruct, and DataCenter classes

--- Plugthon/__init__.py
+++ Plugthon/__init__.py
@@ -1,2 +1,11 @@
-#Import Plugthon version
+# Import the Greetings class for displaying greetings
+from .plugins.hello import Greetings
+
+# Import the SelfDestruct class for downloading one-time media
+from .plugins.timer import SelfDestruct
+
+# Import the DataCenter class for checking data center status
+from .plugins.dc import DataCenter
+
+# Import the version number of the Plugthon package
 from .__version__ import __version__
